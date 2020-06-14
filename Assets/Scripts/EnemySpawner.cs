670292a1b94802762a9215540b62ad87eb92b9b7
Bruno Gehlen <tousepc@gmail.com>
2020-06-14 18:03:12 -0300

    Add enemy spawner. Score animation particles created

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 3f;
+    [SerializeField] Attacker attackerPrefab;
+
+    IEnumerator Start() {
+
+        while (spawn) {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab,transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+}

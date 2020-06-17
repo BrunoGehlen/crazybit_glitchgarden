1b3d69c7a8e8c31415cbb0bbb9aa589b1e6d04ee
Bruno Gehlen <tousepc@gmail.com>
2020-06-17 11:29:03 -0300

    Set up defender selection UI

---
+++ Assets/Scripts/DefenderSelection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSelection : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderSelection>();
+        foreach (DefenderSelection button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 125);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+}

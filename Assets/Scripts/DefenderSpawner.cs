8f62fbbb95d7a233661d0a00c7b6548c4dd82d3e
Bruno Gehlen <tousepc@gmail.com>
2020-06-17 10:51:46 -0300

    Snap defenders to grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class DefenderSpawner : MonoBehaviour
 {
     [SerializeField] GameObject defender;
+    [SerializeField] float offSetY = -0.4f, offSetX = 0f;
     private void OnMouseDown()
     {
         SpawnDefender(GetSquareClicked());
     }
 
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX + offSetX, newY + offSetY);
+    }
     private Vector2 GetSquareClicked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
     }
-    private void SpawnDefender(Vector2 worldPos)
+    private void SpawnDefender(Vector2 roundedPos)
     {
         GameObject newDefender = 
             Instantiate(
                 defender, 
-                worldPos, 
-                Quaternion.identity);
+                roundedPos, 
+                Quaternion.identity) as GameObject;
     }
 }
